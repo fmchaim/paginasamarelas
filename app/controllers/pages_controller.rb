bd7921fd6f7aaf1472d79c7279bf089d43fc7b67
Ylana Oliveira <ylanalmo@gmail.com>
2024-02-02 19:56:10 -0300

    adicionei mapa a homepage, alterei o controller de pages pra isso

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,17 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+
   def home
+    @services = Service.all
+    @markers = @services.geocoded.map do |service|
+      {
+        lat: service.latitude,
+        lng: service.longitude
+      }
+    end
   end
 
   def dashboard
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,4 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true
 
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
 end
